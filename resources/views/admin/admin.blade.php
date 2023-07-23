539e5921e03a7a3f9a68b19bebb807b485bbd3d7
guntursatya25 <guntursatya812@gmail.com>
2023-07-23 17:42:00 +0700

    first admin

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.templates.master')
+
+@section('konten')
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">About Vertical Navbar</h4>
+        </div>
+        <div class="card-body">
+            <p>
+                Vertical Navbar is a layout option that you can use with
+                Mazer.
+            </p>
+
+            <p>
+                In case you want the navbar to be sticky on top while
+                scrolling, add <code>.navbar-fixed</code> class alongside
+                with <code>.layout-navbar</code> class.
+            </p>
+        </div>
+    </div>
+@endsection
